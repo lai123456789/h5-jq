6a38dea24e77aee1c6c5e2ee039a63a749f20b3c
lai123456789 <1849475962@qq.com>
2020-07-30 18:40:56 +0800

    新增答题成功失败分享页面

---
+++ js/zhishitiku.js
@@ -0,0 +1,7 @@
+$(document).on("click",".share",function (e) {
+    $(".main-content").hide().next().show()
+})
+// $(document).on("click",".overlay",function (e) {
+//     $(this).hide()
+//     $(".main-modal").show()
+// })
